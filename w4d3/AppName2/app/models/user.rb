e9942c796caa3cd9b849b02f9967024455fe1097
Ryan Fong <ryanfong@Ryans-MacBook-Pro.local>
2017-02-15 09:42:24 -0800

    Update user model

--- w4d3/AppName2/app/models/user.rb
+++ w4d3/AppName2/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
-    nilf
+    nil
   end
 
   def self.generate_session_token
